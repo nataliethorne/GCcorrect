62f2b655b7411fb156bb2c0f51078e3c3f0ff8df
nataliethorne <thorne@wehi.edu.au>
2013-09-26 13:49:00 +0930

    set up correct_bin_count.py and write_bin_statistics.py

---
+++ write_bin_statistics.py
@@ -0,0 +1,5 @@
+# def write_bin_statistics(bin_position, corrected_bin_count, observed_bin_count, predicted_bin_count, chr)
+	# let chr be an integer
+	# write to a file bin_statistics_"chr",chr.txt
+
+
