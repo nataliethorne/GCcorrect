4c889bbfc9830c03c131af9db1b3cdc287ac1e82
inglon <lonnstedt@wehi.edu.au>
2013-09-26 15:27:51 +0930

    Update python main script

--- gc_correct.py
+++ gc_correct.py
@@ -1,6 +1,12 @@
-#Main python script to gc-correct sequence counts aligned to one #chromosome.
-#
+#Main python script to gc-correct sequence counts aligned to one #chromosome. This script steps through each position in the genome,
+#calculates GC content in a window, estimates GC bias from gc bias
+#function, corrects for the GC bias and then outputs sums of counts
+#and corrected counts in a bin table file.
+
 import sys
+#Mytestrow
+# args = ['chr1.fa','counts_per_position_chr1.txt',
+#'gc_prediction_chr1.txt',1000, 249	]
 
 #Input arguments
 #B = binsize
@@ -13,28 +19,39 @@ countsfile = args[1]
 gcfuncfile = args[2]
 binsize = int(args[3])
 winsize = int(args[4])
-print seqfile + countsfile + gcfuncfile
-print binsize + winsize
+print 'Sequence file: ' +seqfile
+print 'Counts file: ' +countsfile
+print 'GC bias function file: ' +gcfuncfile
+print 'Binsize: ' + str(binsize)
+print 'Window size: ' + str(winsize)
 
+#Init reading and writing files
+print 'Reading gc prediction function.'
+gcreader = open(gcfuncfile,'r')
+line = gcreader.readline()
+while line != '':  #Read all lines of this file
+				  #They are our prediction values
+    line = gcreader.readline()
 
-#Start initalize reading
-reader = open('pg76.txt','r')
-outfile = 
+print 'Start reading fasta file.'
+seqreader = open(seqfile,'r')
+dummy = seqreader.readline()
+print 'Start reading counts file.'
+countsreader = open(countsfile,'r')
+countline = reader.readline()
 
 # initiate bin_position counter (i.e. where we are in a bin)
 # initiate chr_postion in chromosome 
-
-reader = open(seqfile,'r')
-#line = reader.readline()
-
-position_bin = 0 #Index of positions within bins
-position_seq = 0
-print 'Start reading fasta file'
-while line != '':  #While there are more positions to do
+position_bin = 1 #Index of new position within bins (max binsize)
+position_seq = 1 #Index of new position in sequence
+seq_position_in_genome = 0
+sequence = []
+countlist = []
+while seqline != '':  #While there are more positions to do
 	
 	#Read lines of seqfile and countsfile enough for this position
-	res = Readlines()
-	...
+	sequence,position_in_genome = Readlines(position_in_genome=position_seq, sequence, seq_position_in_genome)
+	
 	
  	# get GC for that position 
 	gc = get_position_GC(position, sequence, seqpositions, winsize)
