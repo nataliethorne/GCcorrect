db967bdf92dde01bb34efd93b2388e51605c8f73
nataliethorne <thorne@wehi.edu.au>
2013-09-26 15:31:09 +0930

    plan for Readlines.py function

--- Readlines.py
+++ Readlines.py
@@ -1,3 +1,37 @@
 
-def Readlines(new_position_to_calc, current_fa_start, current_fa_end)
-	pass
+def Readlines(position_in_genome, sequence, seq_position_in_genome, seqreader, countslist, position_in_bin, countsreader)
+
+	# This hasn't bee implemented yet.
+
+	#########
+	# Remove unneccessary sequence in current memory
+	#########
+	# remove positions in sequence that are less than position
+	# if removed above; update sequence_position_in_genome
+
+
+	#########
+	# Read more sequence to do GC window calculation for the current position_in_genome
+	#########
+	# if  seq_position+len(sequence) < position_in_genome+winsize
+	# read next line from reader and append to sequence
+	# update len(sequence)
+	# repeat this block while seq_position+len(sequence) < position_in_genome+winsize
+
+	
+	#########
+	# Read from countsreader to get all counts within the bin
+	#########
+	# if position_in_bin ==0; get counts for the next bin (position_in_genome + binsize)
+
+	#last line read from countsreader
+	#if position in this last line is > than position_in_genome
+	#keep current line in current_bin_counts list
+
+	#if position in current line from counts reader is < position_in_genome+binsize
+	# read next line in countsreader and append to current_bin_counts list
+	# repeat this block until position in counts reader is >= position_in_genome+binsize
+
+	# sum counts in bin store as counts object
+
+	return sequence seq_position_in_genome counts
--- get_position_GC.py
+++ get_position_GC.py
@@ -1,2 +1,25 @@
-def get_position_GC()
-	pass
+def get_position_GC(position_in_genome, sequence, seq_position_in_genome, winsize):
+	'''
+	This function finds the GC content in a window of sequence.
+	The size of the window is given by winsize
+	The GC content in this window will be the GC value attributed to the starting position of the window (i.e. the current position in the genome).
+	Because we only read in parts of the genome sequence at a time, we need to know the genomic position of the input sequence to determine where the genomic position is within the input sequence.
+	'''
+
+
+	# get position in current sequence
+	pos_in_current_sequence= (position_in_genome - seq_position_in_genome) -1
+	
+	# get the sequence in the window for current position
+    # add winsize-1 as indexing positions starts at 0 in python
+	dna=sequence[position_in_current_sequence:(position_in_current_sequence+(winsize-1))] 
+
+	# check for any N's in the reference sequence
+    countN = dna.count('N')
+    dna.replace('N',"")
+    numGs = dna.count('G')
+    numCs = dna.count('C')
+    GC_content = (numGs + numCs)/float(len(dna))
+    return GC_content
+
+
