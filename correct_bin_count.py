ba2b5366ccf540045d070a1dce3924a4f67eefd6
m.phan1@uq.edu.au <swc_trainee@swc-bootcamp-024.(none)>
2013-09-26 14:29:55 +0930

    Add finished correct_bin_count.py and write_bin_statistics.py

--- correct_bin_count.py
+++ correct_bin_count.py
@@ -1,4 +1,7 @@
-# def correct_bin_count(observed_bin_count, predicted_bin_count, bin_position):
-	corrected_bin_count=observed_bin_count/predicted_bin_count
-	write_bin_statistics(bin_position, corrected_bin_count, observed_bin_count, predicted_bin_count)
+# calculate correct_bin_count
+
+def correct_bin_count(observed_bin_count, predicted_bin_count):
+	return corrected_bin_count=observed_bin_count/predicted_bin_count
+
+write_bin_statistics(bin_position_start, bin_position_end, corrected_bin_count, observed_bin_count, predicted_bin_count, chr_number)
 
--- write_bin_statistics.py
+++ write_bin_statistics.py
@@ -1,5 +1,13 @@
-# def write_bin_statistics(bin_position, corrected_bin_count, observed_bin_count, predicted_bin_count, chr)
-	# let chr be an integer
-	# write to a file bin_statistics_"chr",chr.txt
+# write bin statistics to file
 
+filename = 'bin_statistics_chr' + str(chr_number) + '.txt'
+one_line_bin_statistics = open(filename,'w')
+
+def write_bin_statistics(bin_position_start, bin_position_end, corrected_bin_count, observed_bin_count, predicted_bin_count)
+	'''Write bin statistics to file in tab delimited format'''
+	# let chromosome be an integer
+	# write to a file bin_statistics_"chr",chr.txt
+	one_line_bin_statistics.write = (str(bin_position_start) + '-' + str(bin_position_end) + '\t' + str(corrected_bin_count) + '\t' + str(observed_bin_count) + '\t' + str(predicted_bin_count) + '\n')
+	
+one_line_bin_statistics.close()
 
